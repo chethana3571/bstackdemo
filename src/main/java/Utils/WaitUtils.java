ee1a1920a221fd4b228c3c336dfd87f6946e5fd5
Chethana Naik <chethana3571@gmail.com>
2025-12-25 18:24:37 +0530

    https://github.com/chethana3571/bstackdemo.git

--- src/main/java/Utils/WaitUtils.java
+++ src/main/java/Utils/WaitUtils.java
@@ -1,53 +1,24 @@
 package Utils;
-import org.openqa.selenium.By;
+
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
-
-import com.aventstack.extentreports.util.Assert;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class WaitUtils {
 
-    WebDriver driver;
-
-    @BeforeMethod
-    public void setUp() {
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get("https://practicetestautomation.com/practice-test-login/");
+    // Wait for element visibility
+    public static WebElement waitForVisibility(WebDriver driver, By locator, int timeout) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(timeout));
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
     }
 
-    @Test
-    public void loginTestUsingWaitUtils() {
-
-        WaitUtils.waitForVisibility(driver, By.id("username"), 10)
-                .sendKeys("student");
-
-        WaitUtils.waitForVisibility(driver, By.id("password"), 10)
-                .sendKeys("Password123");
-
-        WaitUtils.waitForClickable(driver, By.id("submit"), 10)
-                .click();
-
-        boolean titleStatus = WaitUtils.waitForTitle(driver, "Logged In", 10);
-        Assert.assertTrue(titleStatus, "Login failed");
-    }
-
-    private static boolean waitForTitle(WebDriver driver2, String string, int i) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	private static Object waitForVisibility(WebDriver driver2, By id, int i) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@AfterMethod
-    public void tearDown() {
-        driver.quit();
+    // Wait for title to contain text
+    public static boolean waitForTitle(WebDriver driver, String titleText, int timeout) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(timeout));
+        return wait.until(ExpectedConditions.titleContains(titleText));
     }
 }
